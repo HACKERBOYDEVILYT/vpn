63e1fbe2d09de0b0ad0de95bde45a50be73198d7
DEVIL BOY <94786786+HACKERBOYDEVILYT@users.noreply.github.com>
2026-09-06 13:34:29 +0600

    Create index.ts

---
+++ services/api/src/modules/favorites/index.ts
@@ -0,0 +1,18 @@
+export {
+  registerFavoriteRoutes
+} from "./routes.js";
+
+export {
+  registerFavoritesModule
+} from "./register.js";
+
+export {
+  listFavorites,
+  addFavorite,
+  removeFavorite
+} from "./repository.js";
+
+export {
+  FavoriteServerSchema,
+  type FavoriteServerInput
+} from "./schema.js";
